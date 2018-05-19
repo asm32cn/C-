d9f1ed8896424ecfe561c9f38a39a0b7f7863173
asm32 <asm32@qq.com>
2018-05-19 11:47:15 +0800

    cs-bucket-sort-demo-1--linker.bat ... add

---
+++ C#-Arithmetic-2018/cs-bucket-sort-demo-1.cs/cs-bucket-sort-demo-1--linker.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set strLinker=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\csc.exe
+
+echo %strLinker%
+echo.
+
+set strCmd=%strLinker% /target:exe cs-bucket-sort-demo-1.cs
+echo #%strCmd%
+%strCmd%
+
+pause
