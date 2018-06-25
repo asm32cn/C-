10c6f83cece8ea8a1c0d34a4ecd78916bd2e550c
asm32cn <asm32@qq.com>
2018-06-25 22:42:02 +0800

    cs-sort-tiobe-filename-v3-demo-1--linker.bat ... add

---
+++ C#-Arithmetic-2018/C#-Arithmetic-2018-sort-string/cs-sort-tiobe-filename-demo-1.cs/cs-sort-tiobe-filename-v3-demo-1--linker.bat
@@ -0,0 +1,13 @@
+@echo off
+
+::set strLinker=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\csc.exe
+set strLinker=%SystemRoot%\Microsoft.NET\Framework\v3.5\csc.exe
+
+echo %strLinker%
+echo.
+
+set strCmd=%strLinker% /target:exe cs-sort-tiobe-filename-v3-demo-1.cs
+echo #%strCmd%
+%strCmd%
+
+pause
