b0e5280337a55d6328e2581bfb89fd1b73889036
asm32cn <asm32@qq.com>
2018-06-28 02:41:31 +0800

    AES-Demo-2.cs ... modify

--- c#-2017-training/AES-Demo-2.cs/AES-Demo-2.cs
+++ c#-2017-training/AES-Demo-2.cs/AES-Demo-2.cs
@@ -10,7 +10,7 @@ class AES_Demo_2{
 	// public static byte[] _key1 = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
 	// public static string keys = "dongbinhuiasxiny";//密钥,128位
 	public static byte[] _key1 = { 0xc4, 0xfe, 0xbe, 0xb2, 0xd6, 0xc2, 0xd4, 0xb6, 0xb5, 0xad, 0xb2, 0xb4, 0xc3, 0xf7, 0xd6, 0xbe }; // 宁静致远淡泊明志
-	public static string keys = "0994-18194882682";//密钥,128位  
+	public static string keys = "1a2s3d4f5g6h7j8k";//密钥,128位  
 
 	public static byte[] AESEncrypt(string plainText){
 			SymmetricAlgorithm des = Rijndael.Create();
@@ -51,15 +51,15 @@ Hello aes ! 老子说:“上善若水”，“水善利万物而不争，处众�
 多用来指以崇高的道德、博大精深的学识培育学子成才。清华大学的校训就是“自强不息，
 厚德载物”。
 AESEncrypt:
-Kp9Dy7bj52l5n3PMLZ8HhR79uzr3Ux2IehzLVH56bEekVCz0VEphMW3zzzUKTxgEXFUH8EAtXSsI7hLE
-lvGY+mvUkjeuLsrcNfRsBJ6uRVI7sBIyNfWGuJkGL0n9PA7G+TzGI8dVSd4lkgb+lXxaywMHaoHnBTVZ
-98fndWMeq7qdPkrle3ly4VWEFq0+C3gZKoDTzSBXqqsvCUY6fiLVlAsTwvSN6jEbxSNUzM7vWSRyf+Br
-2zWd3V6Xeb4iy7EFFP3ky5S1zilC9YkX73iriY13nFIy8HcG0raBKcAAZ7pUeCnqmMDyeOYbHgwE+HKb
-nOVYrnxjUloggIR4kv30A1LNhdfNbmovh+jwWh611Os0lUsrosZpFzaOdo17LfX/q1xvL6q+qQK5VJTg
-NbeOj46p1wUSB8DIZ5RzETwY1037uEg7OVTCftvM25Aeb0s+H6HqCSQBvgLWHz0MfU3HVSHdy6dJEr+o
-IZJewOjeeWJcnJEbrt2soiibqB+BL7cVGXIux38HJgupVrFYwLRQUyHxcKpccR4zg6eDTqLM7r5AdRpF
-j5C+oW40PqfxbaVJGoDe4u0X5kza3joqZFa8ANnFc5HJPRkXhUDr/8S0iJuNrcJl6gN8s1/kmZnyAWTm
-ImdEe9+PBfk9CGAQNQe8PkkEKMc4+lo6Advdq3jS69HHYaVgfSh2gRATVf68V6ZO
+1IKPdYfXXvJ8eEuqtSvUrO1OBeuSscR/IX1fhpisxi7ynct+RV9dWj8geCi8DV5jxUyrdmxkQpsiYEtJ
+T4Yk+jw3aAgGXgMxvuJbu3IaIppPA66294vgRhJ+htWtRauBAZFUhNIETNtvR7ZA2bp1mM1g/iGQ6Wfz
+lI3qRwrwi0dEYblJOiR5bN//yT/U+JXuqe4JOBNbr+YS+inFTiqbHpa7MNTiRxKhrwXtjluCudlLlwCE
+bcrjNGZJf9AiqXqF/4plSiGK/dRBdpgzEkfWq+z8LuinsD44ILIlWBVntyUhKRlEmzeXlVKqOa3lzZV3
+6iSP/kLSQ4kYlo7uYY5CmxqdmU1ur3c7UmzK2HBVn+YFBQBclSindHm0RPq0gaQVe4YQBf9m0eCz7Qau
+hgVBhKJunm6zaBEt+oLPll7q1WPB8Lr/AarkRl6kxAOIkIzW6FiP8mkhZjxO0k8ICjgRFZJRkuumX4a7
+wVuKwIkh7/wWb15YJ13HeLOAkpFm/DmHEK34pdrtaXJFrrU60fLYLK+W9KMX84b3qEt/efptbnS1DcNS
+pu/u2EgL3Rbus5vGMnFEsqUGMgzY+RzLoapI2QlQ9FvzBG3T59R3nNsLHT00PxoTlCNmAPT3HEUUJO+u
+xB4ebgSK+E6gBAcut+oZ+BtV0MP7OUBjin8y28HKHYJKab2sqZj3av0gE8BVnjnq
 AESDecrypt:
 Hello aes ! 老子说:“上善若水”，“水善利万物而不争，处众人之所恶，故几于道”。
 这里实际说的是做人的方法，即做人应如水，水滋润万物，但从不与万物争高下，这样的品
