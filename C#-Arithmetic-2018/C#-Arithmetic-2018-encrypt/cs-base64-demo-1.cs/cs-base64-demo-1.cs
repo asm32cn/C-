e6c9db3f69df267624ca030cd02064ad5926b31b
asm32cn <asm32@qq.com>
2018-06-29 23:03:16 +0800

    cs-base64-demo-1.cs ... add

---
+++ C#-Arithmetic-2018/C#-Arithmetic-2018-encrypt/cs-base64-demo-1.cs/cs-base64-demo-1.cs
@@ -0,0 +1,10 @@
+// cs-base64-demo-1.cs
+using System;
+
+class cs_base64_demo_1{
+	public static void Main(string[] args){
+		cs_base64_demo_1 cbd = new cs_base64_demo_1();
+
+		Console.WriteLine("cs-base64-demo-1.cs");
+	}
+}
