0dcc8d03910bed9dd6d6728791d4b5c1b9d91cec
asm32cn <asm32@qq.com>
2018-07-07 12:02:30 +0800

    ButtonTextAlignDemo1.cs ... modify

--- C#-2018-training/C#-2018-training-ui/ButtonTextAlignDemo1.cs/ButtonTextAlignDemo1.cs
+++ C#-2018-training/C#-2018-training-ui/ButtonTextAlignDemo1.cs/ButtonTextAlignDemo1.cs
@@ -5,17 +5,20 @@ using System.Drawing;
 using System.Windows.Forms;
 
 class ButtonTextAlignDemo1 : Form{
-	private Button btn = new Button();
+	private Button[] btnCommands = new Button[2];
 	public ButtonTextAlignDemo1(){
 		this.Text = "ButtonTextAlignDemo1.cs";
 		this.StartPosition = FormStartPosition.CenterScreen;
 		this.Padding = new Padding(10);
 
-		btn.Text = "ButtonTextAlignDemo1.cs";
-		btn.Dock = DockStyle.Top;
-		// btn.TextAlign = CenterLeft;
-		btn.TextAlign = ContentAlignment.MiddleLeft;
-		this.Controls.Add(btn);
+		for(int i = 0; i < 2; i++){
+			btnCommands[i] = new Button();
+			btnCommands[i].Text = string.Format("ButtonTextAlignDemo1.cs - {0}", i);
+			btnCommands[i].Dock = DockStyle.Top;
+			btnCommands[i].TextAlign = ContentAlignment.MiddleLeft;
+
+			this.Controls.Add(btnCommands[i]);
+		}
 	}
 	public static void Main(string[] args){
 		Application.Run(new ButtonTextAlignDemo1());
